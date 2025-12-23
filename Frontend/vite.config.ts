942619461eda7a1dcaf59e063f79978cb72739ec
joxquinnn <joxquinnn1137@gmail.com>
2025-12-22 23:27:29 -0300

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: [
-      'appealing-tranquility-production-3da9.up.railway.app'
-    ]
-  }
+  
 })
