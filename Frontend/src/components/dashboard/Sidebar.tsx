7d73839aa2259f91111b45457bf7e4868fc038dd
joxquinnn <joxquinnn1137@gmail.com>
2026-01-02 05:07:14 -0300

    Update Sidebar.tsx

--- Frontend/src/components/dashboard/Sidebar.tsx
+++ Frontend/src/components/dashboard/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
-import '../styles/HeroSection.styles.css';
+import '../styles/Header.styles.css'; 
 
 const Sidebar: React.FC = () => {
 
